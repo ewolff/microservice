a6a4d110537639c34fad7139053c64a9caa0c500
Eberhard Wolff <eberhard.wolff@gmail.com>
2015-04-02 23:25:44 +0200

    EnableEurekaClient -> EnableDiscoveryClient

--- microservice-demo/microservice-demo-catalog/src/main/java/com/ewolff/microservice/catalog/CatalogApp.java
+++ microservice-demo/microservice-demo-catalog/src/main/java/com/ewolff/microservice/catalog/CatalogApp.java
@@ -5,13 +5,13 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @ComponentScan
 @EnableAutoConfiguration
-@EnableEurekaClient
+@EnableDiscoveryClient
 @Component
 public class CatalogApp {
 
