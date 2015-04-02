5ebc258446b26360f9cb172307a014e03082f4f6
Eberhard Wolff <eberhard.wolff@gmail.com>
2015-04-02 07:32:22 +0200

    Removed Hystrix dashboard from Order application

--- microservice-demo/microservice-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
+++ microservice-demo/microservice-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 public class OrderApp {
 
 	public static void main(String[] args) {
