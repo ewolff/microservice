18c77310faa4aa89c79f31f60d41148617efa165
Eberhard Wolff <eberhard.wolff@gmail.com>
2015-04-12 19:57:58 +0200

    Added @RibbonClient

--- microservice-demo/microservice-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
+++ microservice-demo/microservice-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@RibbonClient("order")
 public class OrderApp {
 
 	public static void main(String[] args) {
